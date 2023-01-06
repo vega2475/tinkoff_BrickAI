a23a92548ca0189cd61eef64770be1fdffef38ad
DavidMSK <devid367552@gmail.com>
2023-01-06 14:44:43 +0300

    v0.03

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 
 /**
 made in Russia
+ Плагин для Microsoft Word 2023 "Выравнивание текста".
  */
 
 public class Main {
@@ -19,26 +20,30 @@ public class Main {
 
     public static void solve(String text, int weight) {
         String[] arrOfParagraph = text.split("\n");
-        String[] arrOfWords = text.split("[. ]");
-        System.out.println(("--------------------------------------------------------------------------"));
+        //String[] arrOfWords = text.split("[. ]");
+        System.out.println(("--------------------------------------------------------------------------"));//это для того чтобы смотреть в консоли ширину текста и ориентироваться по этой штуку (тут 74 "-")
         StringBuilder sb = new StringBuilder();
-        int redString = 6;
-        int space = 1;
 
         for (int i = 0; i < arrOfParagraph.length; i++) {
             String[] arrOfWords1 = arrOfParagraph[i].split(" ");
             int quantityOfSymbols = 6;
             int quantityOfWords = 0;
-            int quantityOfSymbolsInEachParagraph = arrOfParagraph[i].length();
+            int quantityOfSymbolsOfWordsInRow = 0;
+            int quantityOfSymbolsInEachParagraph = arrOfParagraph[i].length();//for debug
+            int quantityOfSpacesInOneRow = 0;
             if(arrOfParagraph[i].length() > weight) {
                 sb.append("      ");
                 for (int j = 0; quantityOfSymbols < weight && j < arrOfWords1.length; j++) {//проблема здесь от квантити до вейт (должно быть меньше из-за последнего слова проблемы)
                     //if(quantityOfSymbols + arrOfWords1[arrOfWords1.length - 1].length() > weight)break;
                     quantityOfSymbols += arrOfWords1[j].length() + 1; //подсчет кол-ва символов для каждой строки включая красную строку (ее пишем как 6 пробелов)
+                    quantityOfSymbolsOfWordsInRow += arrOfWords1[j].length();
                     quantityOfWords++;
+                    quantityOfSpacesInOneRow = weight - quantityOfSymbolsOfWordsInRow;
                     sb.append(arrOfWords1[j]).append(" ");
                     if(j + 1 < arrOfWords1.length) {
                         if (quantityOfSymbols + arrOfWords1[j + 1].length() > weight) {
+                            quantityOfSpacesInOneRow = 0;
+                            quantityOfSymbolsOfWordsInRow = 0;
                             quantityOfSymbols = 0;
                             quantityOfWords = 0;
                             sb.append("\n");
@@ -47,18 +52,7 @@ public class Main {
 
 
                 }
-                //sb.append("\n");
-                int counter = 0;
-//                for(int k = quantityOfWords; k < arrOfWords1.length; k++){
-//                    counter++;
-//                    if(counter <= quantityOfWords) {
-//                        sb.append(arrOfWords1[k]).append(" ");
-//                    }
-//                    else {
-//                        counter = 0;
-//                        sb.append("\n");
-//                    }
-//                }
+
                 sb.append("\n");
             }
             else{
